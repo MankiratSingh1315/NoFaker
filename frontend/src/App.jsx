007379217b6a657bb76bd8a18a7e202351ddfc52
Mankirat Singh <mankiratsingh1315@gmail.com>
2024-04-12 17:16:13 +0530

    add basic frontend and backend, crawler

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LandingPage from './Landing/LandingPage.jsx'
+// import Logo from './assets/logo.jsx'
+
+function App() {
+
+  return (
+    <>
+    <LandingPage/>
+    </>
+  )
+}
+
+export default App
